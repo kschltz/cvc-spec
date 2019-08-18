0736d22bf924ca7ead867bb5da4255f09c320f2a
kaue <schultzkaue@hotmail.com>
2019-08-18 11:59:59 -0300

    initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to cvc-spec-presentation
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/cvc_spec_presentation/core_test.clj
@@ -0,0 +1,7 @@
+(ns cvc-spec-presentation.core-test
+  (:require [clojure.test :refer :all]
+            [cvc-spec-presentation.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
